e9d6fcebdaeed68bc99849e2cc327d9c108f69ce
Gary <gary.p.tse@accenture.com>
2022-12-29 06:15:33 -0800

    merge

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "adventurelooks57843687": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "adventurelooks": {
+            "region": "string",
+            "type": "string"
+        }
+    }
+}
