88c8b8b0dc8390be1907a17cdc2df8f7e14a9293
Gary <gary.p.tse@accenture.com>
2022-12-29 09:58:58 -0800

    pull amplify

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -24,6 +24,8 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
+    "aws_user_files_s3_bucket": "adventurelooks-storage-cce5e25c153114-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1",
     "predictions": {
         "identify": {
             "identifyLabels": {
